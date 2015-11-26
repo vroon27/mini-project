a401451d0d14c392f63d61f4c9240ccbe14de3a9
mayankbhandari10 <mayank10oct@yahoo.com>
2015-11-26 13:29:16 +0530

    Update prog1.cpp

--- prog1.cpp
+++ prog1.cpp
@@ -1,2 +1,8 @@
-#include<iostream.h>
+#include<stdio.h>
 #include<conio.h>
+#include<string.h>
+#include<string.h>
+void main()
+{
+  char a[10],ad[10],label[10],opcode[10],operand[10],symbol[10],ch,d[3]="**";  int st,diff,i,address,add,len,actual_len,finaddr,prevaddr,j=0;
+  char mnemonic[15][15]={"LDA","STA","LDCH","STCH"};
